06e2b31935f3f98e999abbf96e1bc8aac9826597
Monk Wellington <Vishnu@Vishnu.local>
2016-09-02 15:14:07 -0700

    Reconfigure fragment/partial connections. Add links to styles

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 
 // view engine setup
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views/', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
