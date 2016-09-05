36cae1888a647e51dd974e857fd6eefd1ced5121
Monk <forgetmenaught@gmail.com>
2016-09-05 12:43:49 -0700

    bind data to express locals variable

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.locals.appdata = require('./data.json');
+
 app.use('/', routes);
 
 /// catch 404 and forward to error handler
