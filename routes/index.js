8ab3a385b866bd550b2f49ab7523c9c1c9f471ef
Monk <forgetmenaught@gmail.com>
2016-09-06 14:12:04 -0700

    update speakers route

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,39 @@ var router = express.Router();
 var appdata = require('../data.json');
 
 /* GET home page. */
-
 router.get('/', function(req, res) {
-  console.log(appdata);
   var myArtwork = [];
   appdata.speakers.forEach(function (item) {
     myArtwork = myArtwork.concat(item.artwork);
   });
-  console.log(myArtwork);
   res.render('index', {
     title: 'Home',
+    artwork: myArtwork
+  });
+});
+
+/* GET speakers page. */
+router.get('/speakers', function(req, res) {
+  var myArtwork = [];
+  appdata.speakers.forEach(function (item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork
+  });
+});
+
+/* GET speakers page. */
+router.get('/speakers/:speakerid', function(req, res) {
+  var myArtwork = [];
+  appdata.speakers.forEach(function (item) {
+    if (req.params.speakerid === item.shortname) {
+      myArtwork = myArtwork.concat(item.artwork);
+    }
+  });
+  res.render('speakers', {
+    title: 'Speakers',
     artwork: myArtwork
   });
 });
